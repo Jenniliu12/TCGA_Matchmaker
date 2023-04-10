1fadeda4bef85f9d5c6e44be3ec328eff546d890
Jenni Liu <liujenni@umich.edu>
2023-04-09 22:30:56 -0400

    Set up the UI

--- setup.py
+++ setup.py
@@ -2,4 +2,11 @@ from setuptools import setup
 
 setup(name = "TCGA_Matchmaker",
 	version = "0.1.0",
-	packages = ["TCGA_code"])
+	packages = ["TCGA_code"], 
+    entry_points = {
+		'console_scripts': [
+		'test_run = demo_pkg.test:main',
+        'another_module_run = demo_pkg.another_module:main'
+		]
+	}
+	)
